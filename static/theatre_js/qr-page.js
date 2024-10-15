9a79839d9d9a827442d4100f09f441f27ccf3318
guru-sevak-singh <116969988+guru-sevak-singh@users.noreply.github.com>
2024-10-15 13:21:14 +0530

    update changes in theatre applications

---
+++ static/theatre_js/qr-page.js
@@ -0,0 +1,36 @@
+const all_canvas = document.getElementsByClassName('qr-code');
+const protocol = window.location.protocol;
+const host_name = window.location.host;
+
+// Function to generate QR codes for each seat
+function generateQRCode(id, url) {
+    const qrCodeContainer = document.getElementById(id);
+
+    // Check if element and URL are valid
+    if (!qrCodeContainer) {
+        console.error(`Element with ID '${id}' not found.`);
+        return;
+    }
+
+    if (!url) {
+        console.error('Invalid URL provided for QR code generation.');
+        return;
+    }
+
+    QRCode.toCanvas(qrCodeContainer, url, function (error) {
+        if (error) {
+            console.error('QR Code generation error:', error);
+        } else {
+            console.log(`QR code generated for ${url}`);
+        }
+    });
+}
+
+
+for (let i = 0; i < all_canvas.length; i++) {
+    let canvas = all_canvas[i];
+    let seat_id = canvas.id;
+    console.log(seat_id);
+    let url = `${protocol}//${host_name}/theatre/show-menu/${seat_id}`
+    generateQRCode(`${seat_id}`, url)
+}

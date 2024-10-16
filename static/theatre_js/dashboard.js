9f011f85fac7d95053f877928238ea7ebfca46cd
guru-sevak-singh <116969988+guru-sevak-singh@users.noreply.github.com>
2024-10-16 09:25:47 +0530

    creating dashboard js code

---
+++ static/theatre_js/dashboard.js
@@ -0,0 +1,187 @@
+// Global variable to store the chart instance
+let orderComparisonChart;
+
+// Function to create or update the chart
+function createOrUpdateChart(newData) {
+    console.log('New data ==> ', newData)
+    // If the chart already exists, update it
+    if (orderComparisonChart) {
+        // Update the chart's data
+        orderComparisonChart.data.datasets[0].data = [newData.seat_orders, newData.automate_orders];
+        orderComparisonChart.update(); // Update the chart to reflect changes
+    } else {
+        // Create the chart for the first time
+        const ctx1 = document.getElementById('orderComparisonChart').getContext('2d');
+        orderComparisonChart = new Chart(ctx1, {
+            type: 'pie', // Pie chart type
+            data: {
+                labels: ['Table Orders', 'Delivery Orders'],
+                datasets: [{
+                    data: [newData.seat_orders, newData.automate_orders],
+                    backgroundColor: ['rgba(254, 175, 57, 0.6)', 'rgba(44, 64, 110, 0.8)'], // Colors for each section
+                    hoverOffset: 4 // Space when hovering over the segments
+                }]
+            },
+            options: {
+                responsive: true, // Ensures the chart is responsive
+                plugins: {
+                    legend: {
+                        position: 'top', // Position of the legend
+                    },
+                    title: {
+                        display: false,
+                        text: 'Comparison of Table Orders and Delivery Orders' // Chart title
+                    }
+                }
+            }
+        });
+    }
+}
+
+
+// create all years in dropdown
+let year_dropdown = document.getElementById('year-dropdown');
+let running_year = document.getElementById('running-year').innerText;
+console.log(running_year);
+let drop_down_html = ''
+for (let i = 0; i < 6; i++) {
+    drop_down_html += `
+    <a id="year-${running_year}" onclick="ShowRevenueChart(${running_year})" class="dropdown-item">${running_year}</a>
+    `
+    running_year -= 1;
+}
+year_dropdown.innerHTML = drop_down_html;
+
+document.getElementById(`year-${document.getElementById('running-year').innerText}`).classList.add('active')
+
+
+// function which change the graph and get data into the graph
+let revenueChart
+
+function createRevenueChart(data) {
+    let monthlyRevenue = Object.values(data);
+    let labels = Object.keys(data);
+
+    console.log(monthlyRevenue)
+    console.log(labels)
+
+    if (revenueChart) {
+        // Update the chart's data
+        revenueChart.data.datasets[0].data = monthlyRevenue;
+        revenueChart.data.labels = labels
+        revenueChart.update(); // Update the chart to reflect changes
+    }
+    else {
+        // Getting the canvas element
+        const ctx = document.getElementById('revenueChart').getContext('2d');
+
+        // Creating the chart
+        revenueChart = new Chart(ctx, {
+            type: 'bar', // Bar chart type
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Total Revenue (₹)',
+                    data: monthlyRevenue,
+                    backgroundColor: 'rgb(254, 175, 57, 0.6)', // Bar color
+                    borderColor: '#feaf39', // Border color
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true, // Ensures y-axis starts at 0
+                        grid: {
+                            display: false // Removes y-axis grid lines
+                        }
+                    },
+                    x: {
+                        grid: {
+                            display: false // Removes x-axis grid lines
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: false,
+                        text: 'Total Revenue for the Year'
+                    }
+                }
+            }
+        });
+        // revenue chart end
+    }
+}
+
+async function ShowRevenueChart(year) {
+    console.log(year);
+    let url = `/theatre/api/get-yearly-revenue/${year}`
+    let data = await getRequest(url)
+    createRevenueChart(data)
+
+    all_active_tags = year_dropdown.getElementsByClassName('dropdown-item')
+    for (let i = 0; i < all_active_tags.length; i++) {
+        let tag = all_active_tags[i]
+        if (tag.innerText == String(year)) {
+            console.log(tag)
+            tag.setAttribute('class', 'dropdown-item active');
+        }
+        else {
+            tag.setAttribute('class', 'dropdown-item');
+        }
+
+    }
+
+
+    document.getElementById('running-year').innerText = year
+}
+
+ShowRevenueChart(document.getElementById('running-year').innerText)
+
+
+// function to get data using get request
+async function getRequest(url) {
+    console.log(url);
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            return data;
+
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+            throw new Error('Failed to fetch data');
+        });
+}
+
+// getting the data using api
+const api_url = '/theatre/api/dashboard-data'
+
+async function get_all_data() {
+    const date = document.getElementById('daterange').value
+    const complete_url = `${api_url}?daterange=${date}`
+    all_data = await getRequest(complete_url);
+
+    console.log(all_data);
+
+    let total_payment = parseFloat(all_data['Total Payment'].toFixed(2));
+    console.log('payment ===> ', total_payment);
+
+    document.getElementById('table-orders').innerText = all_data.all_orders;
+    document.getElementById('delivery-orders').innerText = all_data.seat_orders;
+    document.getElementById('total-payment').innerText = total_payment;
+    document.getElementById('running-orders').innerText = all_data['running_orders']
+
+    createOrUpdateChart({ seat_orders: all_data.seat_orders, automate_orders: all_data.automate_orders });
+    return all_data
+
+}
+all_data = get_all_data()
+
+document.getElementById('submit-button').addEventListener('click', async (e) => {
+    await get_all_data()
+})

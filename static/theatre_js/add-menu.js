af52e44b2dcfa39be68a2663cdc0ff53aa5824e7
guru-sevak-singh <116969988+guru-sevak-singh@users.noreply.github.com>
2025-01-25 14:17:23 +0530

    add image gallery with application to select images

--- static/theatre_js/add-menu.js
+++ static/theatre_js/add-menu.js
@@ -1,4 +1,48 @@
+async function getRequest(url) {
+
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            return data;
+
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+            throw new Error('Failed to fetch data');
+        });
+}
+
+var selected_food_id = "";
+
+// function to hit the post request
+async function PostRequest(url, data) {
+    csrftoken = document.getElementsByName('csrfmiddlewaretoken')[0].value;
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+        body: JSON.stringify(data),
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data;
+        })
+        .catch(function (error) {
+            console.error('Error:', error);
+            throw new Error('Failed to fetch data');
+        });
+}
+
 const add_url = document.getElementById('item-form').action
+
+var all_images = [];
 
 function OpenAddItem(id) {
     document.getElementById('id_catogary').value = id;
@@ -42,4 +86,59 @@ function DeleteItem(id) {
 function deleteCategory(id) {
     document.getElementById('delete-category-form').action = `/theatre/delete-food-category/${id}`
     $("#deleteCategory").modal("show");
+}
+
+async function openImagePopUp(img, id, food_name) {
+    // get all images from api
+    if (all_images.length == 0) {
+        // get all images
+        let url = `/theatre/api/get-all-images`
+
+        // galery div
+        let modal = document.getElementById('imageModal');
+        let galery = modal.getElementsByClassName('modal-body')[0];
+        let div = document.createElement('div');
+        div.setAttribute('class', 'row');
+
+        let images = await getRequest(url);
+        all_images = images['images'];
+
+        for (let i = 0; i < all_images.length; i++) {
+            let img_src = all_images[i];
+            div.innerHTML += `<div class="col-3">
+            <img src="${img_src}" class="img-thumbnail" alt="image" onclick="UploadImage(this)">
+            </div>
+            `
+            galery.appendChild(div);
+        }
+
+    }
+    selected_food_id = id;
+    
+    document.getElementById('imageModalLabel').innerText = `Select Image from galery for ${food_name}`;
+    // show pupup
+    $('#imageModal').modal('show');
+}
+
+async function UploadImage(img) {
+    food_id = selected_food_id
+    old_food_image = document.getElementById(`food-id-${food_id}`)
+    // hit upload image api
+    let url = `/theatre/api/upload-food-image`
+    let upload_data = {
+        'food_id': food_id,
+        'image_url': img.src
+    }
+    console.l
+    data = await PostRequest(url, upload_data);
+    
+    if (data['message'] == 'Image Uploaded') {
+        // update image
+        old_food_image.src = img.src;
+    }
+    else {
+        alert(data['message']);
+    }
+
+    $('#imageModal').modal('hide');
 }
